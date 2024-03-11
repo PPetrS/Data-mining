a161860bb8a57515027772c9a932f7508bfa7a41
Petr Šimeček <petrinsim@gmail.com>
2024-03-11 14:03:51 +0100

    Update XML_parse.py

--- XML_parse.py
+++ XML_parse.py
@@ -4,3 +4,8 @@ import csv
 # Open xml file
 tree = ET.parse('nazev_xml_souboru.xml')
 root = tree.getroot()
+
+# Otevření CSV souboru pro zápis
+with open('vystupni_csv_soubor.csv', 'w', newline='', encoding='utf-8') as csvfile:
+# Vytvoření objektu pro zápis do CSV
+csvwriter = csv.writer(csvfile)
